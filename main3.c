0ccc5c93d301d0364254899e2b61d73bf88ce1bf
amang33 <79344267+amang33@users.noreply.github.com>
2023-03-27 22:50:19 +0900

    Create main3.c

---
+++ main3.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+int main(void) {
+////	true 	= (0이 아닌 모든 값)
+////	false 	= (0)
+////	&		= 변수의 주소를 찾기 위해 존재 (이후에 자세히 설명해주신다 함) 
+
+////	scanf	= 입력
+////	printf	= 출력 
+
+//	int num;
+
+//	scanf("%d", &num);
+//	printf("%d", num);	
+
+////	===========================================================
+
+////	2개의 값을 입력받아 빼기, 곱하기 값을 구하시오 
+//	int num1, num2;
+//	
+//	printf("정수 2개 입력 : ");
+//	
+//	scanf("%d %d", &num1, &num2);
+//	
+//	printf("%d - %d = %d\n", num1, num2, num1 - num2);
+//	printf("%d * %d = %d\n", num1, num2, num1 * num2);
+
+////	===========================================================
+
+////	직육면체의 부피를 구하시오. 
+//	int width, length, height, volume = 0;
+//	
+//	printf("직육면체의 부피를 계산하는 프로그램\n");
+//	
+//	printf("가로 : ");
+//	scanf("%d", &width);
+//	
+//	printf("세로 : ");
+//	scanf("%d", &length);
+//	
+//	printf("높이 : ");
+//	scanf("%d", &height);
+//	
+//	volume = width * length * height;
+//	
+//	printf("직육면체의 부피 : %d \n", volume);
+
+////	===========================================================
+
+//	int num = 5 + 3 * 8 / 2 - 10;
+	
+//	printf("%d", num);
+//	printf("%d", 5 + 3 * 8 / 2 - 10);
+
+
+
+//	int num1 = 2, num2 = 4, num3 = 6;
+//	
+//	printf("%d", num1 + num2 + num3);
+
+
+
+////	숫자가 아닌 값을 입력시 에러가 나오지 않음 (알 수 없음)  
+//	int num1, num2, num3;
+//	
+//	scanf("%d %d %d", &num1, &num2, &num3);
+//	printf("%d", num1 + num2 + num3);
+
+
+
+//	int num1, num2;
+//
+//	scanf("%d %d", &num1, &num2);
+//	printf("%d %d", num1 / num2, num1 % num2);
+	return 0;
+}
